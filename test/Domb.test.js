bbc3f18fa823eca9433d5a1d192de6e96a164e14
sahil-raj <sahilrajaaru@gmail.com>
2024-03-06 10:41:13 +0530

    deployment test

--- test/Domb.test.js
+++ test/Domb.test.js
@@ -1,16 +1,25 @@
 const {expect} = require("chai");
 const {ethers} = require("hardhat");
 
+//one describe section for whole Domb.test.js
 describe("domb tests" , () => {
     let Domb, add1, add2, owner, hardhatToken;
 
+    //hook to perform step before each test
     beforeEach(async () => {
+        //get an instance of the contract
         Domb = await ethers.getContractFactory("Domb");
+        //get the signers
         [owner, add1, add2] = await ethers.getSigners();
+        //deploy the contract to the local hardhat chain
         hardhatToken = await Domb.deploy();
     });
 
+    //tests related to the deployment of the contract to the chain
     describe("deployment", async () => {
-
+        //test to check wheter the owner and the person deploying is same
+        it("Should assign deploying person as owner", async () => {
+            expect(await hardhatToken.owner()).to.equal(owner.address);
+        });
     });
 });
